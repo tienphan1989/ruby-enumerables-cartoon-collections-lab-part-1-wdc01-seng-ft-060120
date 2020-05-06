343bf07c5fd279fca5a2f4af6cc5355758cb5b1d
Tien Phan <tienphan1989@gmail.com>
2020-05-06 20:45:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |value|
-   puts "#{oppressed_worker.capitalize} is welcome!"
+   puts "#{value} is welcome!"
 end
 end
 
@@ -11,4 +11,5 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  
 end
