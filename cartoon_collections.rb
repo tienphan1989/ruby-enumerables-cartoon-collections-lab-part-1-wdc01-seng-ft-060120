55cdb5b85f757a0a8238fbd3ef8dc67151060e0a
Tien Phan <tienphan1989@gmail.com>
2020-05-06 20:51:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each.with_index do |value, index|
-    puts "{index}{value}"
+    puts "#{index}#{value}"
   end
 end
